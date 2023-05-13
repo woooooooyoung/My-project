3f351c96c5a5d7c323c1e10123ef1d849ba6a1bc
WOO YOUNG PARK <dhdhdud1@naver.com>
2023-05-13 15:03:58 +0900

    새로시작

---
+++ Assets/Configs/PlayerController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Experimental.AI;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector3 moveDir;
+
+    [SerializeField]
+    private float movePower;
+
+    [SerializeField]
+    private float moveSpeed; 
+
+    [SerializeField]
+    private float jumpPower;
+
+    [SerializeField]
+    private float rotateSpeed;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        Move();
+        //Rotate();
+        // LookAt();
+    }
+
+    private void Move()
+    {
+        rb.AddForce(moveDir * movePower, ForceMode.Force); // 가고싶은 방향에 무게 싣기
+        transform.Translate(moveDir * moveSpeed * Time.deltaTime, Space.Self); // 자기 기준으로 회전
+        transform.position += moveDir * Time.deltaTime; // x : 1/s 한 프레임당 걸리는 시간에 대해 곱해줘야 속력으로 움직일 수 있음
+        // transform.Translate(moveDir * moveSpeed *  Time.deltaTime); // 바라보는 방향으로 회전
+        // transform.Translate(moveDir * moveSpeed *  Time.deltaTime , Space.World); // 월드기준으로 회전
+    }
+    private void Rotate()
+    {
+        transform.Rotate(Vector3.up, moveDir.x * rotateSpeed * Time.deltaTime);
+    }
+
+    public void LookAt() // 목적지를 지정해주면 바로 감
+    {
+        // transform.LookAt(new Vector3(0, 0, 0));
+    }
+    public void Rotation()
+    {
+        transform.position = new Vector3(0, 0, 0);
+        // transform.rotation = new Quaternion.Euler(0, 0, 0);
+        // Vector3 rotation = transform.rotation.eulerAngles();
+    }
+    private void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse); // 가고싶은 방향에 무게 싣기
+    }
+
+    private void OnMove(InputValue value)
+    {
+        moveDir.x = value.Get<Vector2>().x; // 왼쪽 오른쪽
+        moveDir.z = value.Get<Vector2>().y; // 위 아래
+        Debug.Log(moveDir);
+    }
+    private void OnJump(InputValue value)
+    {
+        Jump();
+    }
+
+}
---
+++ Assets/Scripts/Unity/UnityScript.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class UnityScript : MonoBehaviour
+{
+
+    /************************************************************************
+    * 컴포넌트 (Component)
+    * 
+    * 특정한 기능을 수행할 수 있도록 구성한 작은 기능적 단위
+    * 게임오브젝트의 작동과 관련한 부품
+    * 게임오브젝트에 추가, 삭제하는 방식의 조립형 부품
+    * 컴포넌트를 쓴다고 하면 스크립트를 작성한다고 생각하면 됨
+    ************************************************************************/
+
+    /************************************************************************
+	 * MonoBehaviour
+	 * 
+	 * 컴포넌트에 더 많은 기능을 담은게 MonoBehaviour라고 생각하면 됨
+	 * 게임 오브젝트에 스크립트를 컴포넌트로서 연결할 수 있는 구성을 제공
+	 * 스크립트 직렬화 기능, 유니티메시지 이벤트를 받는 기능, 코루틴 기능을 포함함
+	 ***********************************************************************/
+
+    // <스크립트 직렬화 기능>
+    // 인스펙터 창에서 컴포넌트의 맴버변수 값을 확인하거나 변경하는 기능
+    // 컴포넌트의 값형식 데이터를 확인하거나 변경
+    // 컴포넌트의 참조형식 데이터를 드래그 앤 드랍 방식으로 연결
+
+    // <인스펙터창 직렬화가 가능한 자료형>
+    // private가 아닌 public를 쓰는 이유는 인스펙터창에서 안보임
+    [Header("C# Type")]
+    // 
+    public bool boolValue;
+    // 
+    public int intValue;
+    //  
+    public float floatValue;
+    // 
+    public string stringValue;
+    // 그 외 기본 자료형
+
+    // 기본 자료형의 선형자료구조
+    public int[] array;
+    // 
+    public List<int> list;
+    // 
+
+    [Header("Unity Type")]
+    public Vector3 vector3; // x y z vector2는 2차원 배열로 x y만 가지고 있음
+    // 
+    public Color color;
+    // 원하는 색을 넣을 수 있음
+    public LayerMask layerMask;
+    // 
+    public AnimationCurve curve;
+    // 원하는 수치를 곡선으로 가질 수 있음
+    public Gradient gradient;
+    // 색에 대해서 여러가지 연속적인 색을 가질 수 있음
+
+    [Header("Unity GameObject")]
+    // 
+    public GameObject obj;
+    // 
+
+    [Header("Unity Component")]
+    // 
+    public new Transform transform;
+    // 
+    public new Rigidbody rigidbody;
+    // 질량
+    public new Collider collider;
+    // 충돌체
+
+    [Header("Unity Event")]
+    // 
+    public UnityEvent OnEvent;
+    // 
+
+    // <어트리뷰트>
+    // 클래스, 프로퍼티 또는 함수 위에 명시하여 특별한 동작을 나타낼 수 있는 마커
+    // 어트리뷰트는 대괄호[]에다가 작성함
+    // 영향을 주는 소스는 아니지만 여러가지 정보를 제공함
+
+    [Space(30)] // 30만큼의 공간에서 떨어짐, 빈칸을 넣어줌
+
+    [Header("Unity Attribute")] // 제목
+    [SerializeField] // private지만 인스펙터창에서 보이게 함
+    private int privateValue; // 외부클래스에서 접근불가, 하지만 인스펙터창에선 접근 가능
+    [HideInInspector] // public지만 인스펙터창에서 안보이게 함
+    public int publicValue;
+
+    [Range(0, 10)] // 0~10 사이의 슬라이드 바
+    public float rangeValue;
+
+    [TextArea(3, 5)] // 여러줄 써야하는 대화지를 쓸 수 있게함, 여러줄의 텍스트를 입력할 수 있음
+    public string textField;
+}
